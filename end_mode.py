d6f7d3578525578343502b2df26fdc57976d01c5
seolvin21 <jangheeju0404@tukorea.ac.kr>
2023-12-03 18:26:43 +0900

    created end_mode

---
+++ end_mode.py
@@ -0,0 +1,36 @@
+from pico2d import *
+import game_framework
+import loading_mode
+import start_mode
+
+def init():
+    # global image
+    # image = load_image('game_end.png')
+    pass
+
+
+def finish():
+    pass
+
+
+def update():
+    pass
+
+
+def draw():
+    clear_canvas()
+    # image.draw(400, 300)
+    update_canvas()
+    pass
+
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
+            game_framework.change_mode(start_mode)
+    pass
